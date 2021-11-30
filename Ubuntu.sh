95ee958631fb63a6ff668c890afd41a6d172bef4
internal77 <sumyagel@gmail.com>
2021-11-30 20:25:56 +0100

    r

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -181,3 +181,33 @@ sudo nano /etc/postfix/main.cf
 sudo postconf > /dev/null
 sudo systemctl restart postfix
 sudo nano /etc/dovecot/conf.d/20-lmtp.conf - # https://doc.dovecot.org/configuration_manual/howto/postfix_dovecot_lmtp/
+#--------------настройка и установка-------------------------#
+# https://www.linode.com/docs/guides/email-with-postfix-dovecot-and-mysql/
+sudo mysql
+CREATE DATABASE mailserver;
+CREATE USER 'mailuser'@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT ON mailserver.* TO 'mailuser'@'localhost';
+FLUSH PRIVILEGES;
+USE mailserver;
+CREATE TABLE `virtual_domains` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `virtual_users` (
+  `id` int(11) NOT NULL auto_increment,
+  `domain_id` int(11) NOT NULL,
+  `password` varchar(106) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `virtual_aliases` (
+  `id` int(11) NOT NULL auto_increment,
+  `domain_id` int(11) NOT NULL,
+  `source` varchar(100) NOT NULL,
+  `destination` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
