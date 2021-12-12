a5308491908240af810aeb2695c63d4b3222472b
internal77 <sumyagel@gmail.com>
2021-12-12 15:31:01 +0100

    r

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -10,30 +10,39 @@
 # tar -czvf /backup/mysql/$(date +%d-%m-%Y)/wordpress.tar.gz /home/centos/wordpress_backup.sql
 # find /backup/mysql/* -mtime +30 -exec rm {} \;
 # exit 0
+#!/bin/bash
+
 # DUMP SQL BASE
-mysqldump -u root -p6oWN.bzOIOIw!? wordpress_db > /home/centos/wp.sql
+
+mysqldump -u root -p6oWN.bzOIOIw!? wordpress_db > /home/centos/temp/wp.sql
 echo DUMP wp.sql COMPLITED!!!!
 sleep 1
-mysqldump -u root -p6oWN.bzOIOIw!? drupal_db > /home/centos/dp.sql
+mysqldump -u root -p6oWN.bzOIOIw!? drupal_db > /home/centos/temp/dp.sql
 echo DUMP dp.sql COMPLITED!!!!
 sleep 1
+
 # COMPRESSION
+
 echo Wait pls! Compression All Files...
 sleep 1
 mkdir -p /backup/mysql/`date +%d-%m-%Y`
-tar -czvf /backup/mysql/`date +%d-%m-%Y`/wp.tar.gz  /home/centos/wp.sql
+tar -czvf /backup/mysql/`date +%d-%m-%Y`/wp.tar.gz  /home/centos/temp/wp.sql
 echo COMPRESSION WORDPRESS COMPLITED!!!!
-tar -czvf /backup/mysql/`date +%d-%m-%Y`/dp.tar.gz /home/centos/dp.sql
+tar -czvf /backup/mysql/`date +%d-%m-%Y`/dp.tar.gz /home/centos/temp/dp.sql
 echo COMPRESSION DRUPAL COMPLITED!!!!
 echo COMPRESSION COMPLITED!!!!
 sleep 1
+
 # DELETE TEMP FILE
+
 echo Wait PLS! CLEAR TEMP FILES!!!!
-rm /home/centos/wp.sql dp.sql
+rm /home/centos/temp/*
 echo TEMP files delete comlete!!!!
-find /backup/mysql/`date +%d-%m-%Y` -mtime +30 -exec rm -rf {} \;
-# find /backup/mysql/ -maxdepth 1 -type -d -mtime +30 -exec rm -rf {} \;
+find /backup/mysql/* -mtime +30 -exec rm -rf {} \;
 exit 0
+
+# find /backup/mysql/ -maxdepth 1 -type -d -mtime +30 -exec rm -rf {} \;
+
 # crontab -e - создание задания
 # crontab -l > /path/to/file
 # crontab -u user -l > /path/to/file
